62f95ea9511b80079ab095571317135bdb6773ec
Guilherme Oliveira <guilherme.rhema@hotmail.com>
2024-04-04 12:53:04 -0300

    intensivao exercicio JS

---
+++ main.js
@@ -0,0 +1,91 @@
+// question one // 
+
+alert("Hello World!");
+
+// question two//
+
+const numberOne = 17
+const numberTwo = 25
+
+sum = Number(numberOne + numberTwo);
+alert(sum);
+
+// question three //
+
+const numberOrNot = 56
+
+if (typeof numberOrNot == 'number'){
+    alert(`É um número`)
+} else {
+    alert(`Não é um número`)
+}
+
+// question four //
+
+const stringOrNot = 84
+
+if (typeof stringOrNot == 'string'){
+    alert(`É uma string`)
+} else {
+    alert(`Não é uma string`)
+}
+
+// question five // 
+
+const isBooleanOrNot = false
+
+if (typeof isBooleanOrNot == 'boolean'){
+    alert(`É um booleano`)
+} else {
+    alert(`Não é um booleano`)
+}
+
+// question six // 
+
+const numberThree = 12
+const numberFour = 2
+
+subtraction = Number(numberThree - numberFour)
+
+alert(subtraction);
+
+// question seven // 
+
+const numberFive = 6
+const numberSix = 2
+
+multiplication = Number(numberFive * numberSix)
+
+alert(multiplication)
+
+// question eight // 
+
+const numberSeven = 9
+const numberEight = 3
+
+division = Number(numberSeven / numberEight)
+
+alert(division);
+
+// question nine // 
+
+const pairOrNot = 8
+const resultPairOrNot = Number(pairOrNot % 2)
+
+if (resultPairOrNot == 0){
+  alert(`É um número par`)  
+} else {
+    alert(`Não é um número par`)
+}
+
+// question ten //
+
+const oddOrNot = 5
+const resultOddOrNot = Number(oddOrNot % 2)
+
+if(resultOddOrNot != 0){
+    alert(`É um número ímpar`)
+} else {
+    alert(`Não é um número ímpar`)
+}
+
